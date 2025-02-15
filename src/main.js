3cdafe01fec93df7b20539a47cec9fb076ec573b
Lucian Bornaz <lucian.bornaz@outlook.com>
2025-02-15 16:24:12 +0000

    use node import style

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-import { info, setOutput, setFailed } from "@actions/core"
-import * as tagHelper from './tag-helper'
+const core = require('@actions/core');
+const generateNextTag = require('../src/tag-helper');
 
 function main() {
   try {
     const current_tag = process.env.INPUT_CURRENT_TAG
     const preserve_v = process.env.INPUT_PRESERVE_V
-    const nextTag = tagHelper.generateNextTag(current_tag, preserve_v)
+    const nextTag = generateNextTag(current_tag, preserve_v)
 
-    info(`nextTag=${nextTag}`)
-    setOutput("tag", nextTag);
+    core.info(`nextTag=${nextTag}`)
+    core.setOutput("tag", nextTag);
   }
   catch (error) {
-    setFailed(error);
+    core.setFailed(error);
   }
 }
 
